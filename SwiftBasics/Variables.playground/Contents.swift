f83b6da55969a67538c3aa0413fb7f7d17fef5d0
Raymond Sutton <raymond.sutton.15@cnu.edu>
2018-12-17 14:47:56 -0500

    Add files via upload

---
+++ SwiftBasics/Variables.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Operators
+
+//Unary, Binary, Ternary
+var amICool = true
+amICool = !amICool
+var feelGoodAboutMyself = amICool ? true : false
+
+var bankAccountBalance = 100
+var cashRegisterMessage = bankAccountBalance >= 50 ? "You just bought the item!" : "You are broke, my dude."
+
