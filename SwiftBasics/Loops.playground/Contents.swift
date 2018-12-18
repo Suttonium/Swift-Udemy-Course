ea989c8fe7e59f06a9bc9ff54f75f513b2063b20
Raymond Sutton <raymond.sutton.15@cnu.edu>
2018-12-18 11:54:54 -0500

    Add files via upload

---
+++ SwiftBasics/Loops.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var salaries = [45000.0, 100000.0, 54000.0, 20000.0]
+
+var index = 0
+repeat {
+    salaries[index] *= salaries[index] * 0.10
+    index += 1
+} while (index < salaries.count)
+
+for i in 0..<salaries.count {
+    salaries[i] *= salaries[i] * 0.10
+}
+
+for salary in salaries {
+    print("Salary: \(salary)")
+}
