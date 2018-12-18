04a0361fcafd24383e484607ffaef72d4034a7a0
Raymond Sutton <raymond.sutton.15@cnu.edu>
2018-12-18 11:24:47 -0500

    Add files via upload

---
+++ SwiftBasics/Arrays.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//var employeeSalaries: [Double]= [50000.0, 65000.0, 75000.0] also works
+var employeeSalaries = [50000.0, 65000.0, 75000.0]
+employeeSalaries.append(60000) // add to array
+employeeSalaries.remove(at: 1) // remove from a specific index
+
+
+var students = [String]() // create a blank array of type String
+
+
